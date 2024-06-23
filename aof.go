4d705c33e8cbaa3e79d3a71d47585cb237388370
Richard <richard.tabaka@outlook.com>
2024-06-22 18:26:30 -0700

    Add persistence

--- aof.go
+++ aof.go
@@ -1,1 +1,89 @@
 package main
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"sync"
+	"time"
+)
+
+type AppendOnlyFile struct {
+	file  *os.File
+	rd    *bufio.Reader
+	mutex sync.Mutex
+}
+
+var logCommands = map[string]bool{
+	"GET":     false,
+	"HGET":    false,
+	"HGETALL": false,
+	"HSET":    true,
+	"PING":    false,
+	"SET":     true,
+}
+
+func NewAof(path string) (*AppendOnlyFile, error) {
+	// create or open file for backup
+	openFile, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		return nil, err
+	}
+
+	appendOnlyFile := &AppendOnlyFile{
+		file: openFile,
+		rd:   bufio.NewReader(openFile),
+	}
+
+	// goroutine that will sync to disk every 5 seconds
+	go func() {
+		for {
+			appendOnlyFile.mutex.Lock()
+			appendOnlyFile.file.Sync()
+			appendOnlyFile.mutex.Unlock()
+			time.Sleep(time.Second * 5)
+		}
+	}()
+
+	return appendOnlyFile, nil
+}
+
+// Method to safely close file on server shutdown
+func (appendOnlyFile *AppendOnlyFile) CloseFile() error {
+	appendOnlyFile.mutex.Lock()
+	defer appendOnlyFile.mutex.Unlock()
+
+	return appendOnlyFile.file.Close()
+}
+
+// Read contents of file into memory
+func (appendOnlyFile *AppendOnlyFile) Read(fn func(value Value)) error {
+	appendOnlyFile.mutex.Lock()
+	defer appendOnlyFile.mutex.Unlock()
+
+	appendOnlyFile.file.Seek(0, io.SeekStart)
+
+	reader := NewResp(appendOnlyFile.file)
+
+	for {
+		value, err := reader.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		fn(value)
+	}
+	return nil
+}
+
+// Method to write commands to a file for persistence
+func (appendOnlyFile *AppendOnlyFile) Write(value Value) error {
+	appendOnlyFile.mutex.Lock()
+	defer appendOnlyFile.mutex.Unlock()
+
+	_, err := appendOnlyFile.file.Write(value.Marshal())
+
+	return err
+}
--- main.go
+++ main.go
@@ -8,14 +8,34 @@ import (
 )
 
 func main() {
-	fmt.Println("Listening on port :6379")
-
 	// Create server
 	listener, err := net.Listen("tcp", ":6379")
 	if err != nil {
 		fmt.Println("Listener creation error: ", err.Error())
 		os.Exit(1)
 	}
+	fmt.Println("Listening on port :6379")
+
+	// Create AppendOnlyFile for persistence
+	appOnlyFile, err := NewAof("database.aof")
+	if err != nil {
+		fmt.Println("AppendOnlyFile creation error: ", err.Error())
+		os.Exit(1)
+	}
+	defer appOnlyFile.CloseFile()
+
+	// load contents of file to memory
+	appOnlyFile.Read(func(value Value) {
+		command := strings.ToUpper(value.array[0].bulk)
+		args := value.array[1:]
+
+		handler, ok := Handlers[command]
+		if !ok {
+			fmt.Println("Invalid command: ", command)
+			return
+		}
+		handler(args)
+	})
 
 	// Listen for connections
 	connection, err := listener.Accept()
@@ -54,6 +74,11 @@ func main() {
 			fmt.Println("Invalid command: ", command)
 			writer.Write(Value{valueType: "string", str: ""})
 			continue
+		}
+
+		// persist commands
+		if logCommands[command] {
+			appOnlyFile.Write(value)
 		}
 
 		result := handler(args)
